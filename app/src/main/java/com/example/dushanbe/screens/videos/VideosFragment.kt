58fea84ef34ebd9f3d56ceb003017cfdb0ef04fa
bunyod <b.bekmurodov@alif.mobi>
2022-06-14 16:35:22 +0500

    small fix

--- app/src/main/java/com/example/dushanbe/screens/videos/VideosFragment.kt
+++ app/src/main/java/com/example/dushanbe/screens/videos/VideosFragment.kt
@@ -3,6 +3,10 @@ package com.example.dushanbe.screens.videos
 
 
 import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.util.TimeUtils
+import android.view.View
 import androidx.core.os.bundleOf
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -12,6 +16,8 @@ import com.example.dushanbe.screens.MainActivity
 import com.example.dushanbe.utils.base.BaseFragment
 import com.example.dushanbe.utils.ui.navigate
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
@@ -32,24 +38,35 @@ class VideosFragment : BaseFragment<VideosLayoutBinding,VideosViewModel>() {
         binding.engWrapper.setOnClickListener { (activity as MainActivity).setLocale("tg") }
         binding.ruWrapper.setOnClickListener { (activity as MainActivity).setLocale("ru") }
 
-        CoroutineScope(lifecycleScope.coroutineContext).launch {
-            val oneRaw=requireContext().resources.getIdentifier("touristic_place","raw",requireContext().packageName)
-            val localUri = Uri.parse("android.resource://" + requireContext().packageName + "/" + oneRaw)
-            binding.one.setVideoURI(localUri)
-            binding.one.seekTo(15)
-        }
-        CoroutineScope(lifecycleScope.coroutineContext).launch {
+           val oneRaw=requireContext().resources.getIdentifier("touristic_place","raw",requireContext().packageName)
+           val localUri = Uri.parse("android.resource://" + requireContext().packageName + "/" + oneRaw)
+           binding.one.setVideoURI(localUri)
+
+
+
             val twoRaw=requireContext().resources.getIdentifier("opportunity_dushanbe","raw",requireContext().packageName)
             val localUriTwo = Uri.parse("android.resource://" + requireContext().packageName + "/" + twoRaw)
             binding.two.setVideoURI(localUriTwo)
-            binding.two.seekTo(15)
-        }
-        CoroutineScope(lifecycleScope.coroutineContext).launch {
+
+
             val threeRaw=requireContext().resources.getIdentifier("dushanbe","raw",requireContext().packageName)
             val localUriThree = Uri.parse("android.resource://" + requireContext().packageName + "/" + threeRaw)
             binding.three.setVideoURI(localUriThree)
-            binding.three.seekTo(15)
-        }
+
+
+
+//        CoroutineScope(lifecycleScope.coroutineContext).launch {
+//            val twoRaw=requireContext().resources.getIdentifier("opportunity_dushanbe","raw",requireContext().packageName)
+//            val localUriTwo = Uri.parse("android.resource://" + requireContext().packageName + "/" + twoRaw)
+//            binding.two.setVideoURI(localUriTwo)
+//            binding.two.seekTo(15)
+//        }
+//        CoroutineScope(lifecycleScope.coroutineContext).launch {
+//            val threeRaw=requireContext().resources.getIdentifier("dushanbe","raw",requireContext().packageName)
+//            val localUriThree = Uri.parse("android.resource://" + requireContext().packageName + "/" + threeRaw)
+//            binding.three.setVideoURI(localUriThree)
+//            binding.three.seekTo(15)
+//        }
 
 
 
@@ -65,6 +82,13 @@ class VideosFragment : BaseFragment<VideosLayoutBinding,VideosViewModel>() {
             it.navigate(R.id.action_videosToVideoFragment, bundleOf(Pair(VIDEO_NAME,"dushanbe")))
         }
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        binding.one.seekTo(300)
+//        binding.two.seekTo(10)
+//        binding.three.seekTo(30)
     }
 
 
